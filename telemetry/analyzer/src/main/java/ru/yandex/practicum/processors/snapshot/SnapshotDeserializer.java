9de2ea93fa0e562db53c0939fe03af216672540b
Yuriy <y8789727@yandex.ru>
2025-11-20 11:57:02 +0300

    Спринт 20 Analyzer

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/processors/snapshot/SnapshotDeserializer.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.processors.snapshot;
+
+import ru.yandex.practicum.kafka.deserializer.BaseAvroDeserializer;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+public class SnapshotDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
+    public SnapshotDeserializer() {
+        super(SensorsSnapshotAvro.getClassSchema());
+    }
+}
